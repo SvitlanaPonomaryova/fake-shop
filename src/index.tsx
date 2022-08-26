26a71cb0735d627657afbd11f8986fb6ce239127
Svitlana Ponomaryova <Svetapriluky@gmail.com>
2022-08-26 09:34:35 +0200

    add React Fragment

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const h1 = <h1>Hello!</h1>
-
 function  App() {
     return (
-        <div>
-            {h1}
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex cupiditate quia nostrum voluptates, quasi saepe porro atque ducimus obcaecati esse minima distinctio delectus, temporibus corporis nam! Ipsum, quos perspiciatis? Accusantium?</p>
-        </div>
+        <>
+            <div>
+                <h1>Hello world</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex cupiditate quia nostrum voluptates, quasi saepe porro atque ducimus obcaecati esse minima distinctio delectus, temporibus corporis nam! Ipsum, quos perspiciatis? Accusantium?</p>
+            </div>
+            <div>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum quibusdam nemo fugiat dolorem soluta autem labore. Iure, praesentium dolore. Officiis sed quis facere voluptatibus cumque saepe dicta assumenda labore omnis.</p>
+            </div>
+            <div>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur molestias, harum sint minus reprehenderit sunt, numquam id totam at voluptatem vel? Inventore fugit voluptatibus commodi maxime pariatur neque laborum possimus.</p>
+            </div>
+        </>
     )
   
 }
