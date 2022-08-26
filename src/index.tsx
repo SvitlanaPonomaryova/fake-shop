a7d8ee4d38a8a0f713402528192881b4fe662812
Svitlana Ponomaryova <Svetapriluky@gmail.com>
2022-08-26 07:37:28 +0200

    add test App

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const h1 = <h1>Hello!</h1>
+
+function  App() {
+    return (
+        <div>
+            {h1}
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex cupiditate quia nostrum voluptates, quasi saepe porro atque ducimus obcaecati esse minima distinctio delectus, temporibus corporis nam! Ipsum, quos perspiciatis? Accusantium?</p>
+        </div>
+    )
+  
+}
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render("Hello world!");
+root.render(<App/>);
 
 
