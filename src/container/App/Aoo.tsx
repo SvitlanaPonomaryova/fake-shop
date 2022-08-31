9a9281807787027be793e6cdf21e4298d3e46318
Svitlana Ponomaryova <Svetapriluky@gmail.com>
2022-08-31 18:46:54 +0200

    add absolute import and dummy App

---
+++ src/container/App/Aoo.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+const Aoo = (props: Props) => {
+    return <div>Aoo</div>
+}
+
+export default Aoo
--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './container/App/Aoo'
 
-function  App() {
-    return (
-        <>
-            <div>
-                <h1>Hello world</h1>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex cupiditate quia nostrum voluptates, quasi saepe porro atque ducimus obcaecati esse minima distinctio delectus, temporibus corporis nam! Ipsum, quos perspiciatis? Accusantium?</p>
-            </div>
-            <div>
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum quibusdam nemo fugiat dolorem soluta autem labore. Iure, praesentium dolore. Officiis sed quis facere voluptatibus cumque saepe dicta assumenda labore omnis.</p>
-            </div>
-            <div>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur molestias, harum sint minus reprehenderit sunt, numquam id totam at voluptatem vel? Inventore fugit voluptatibus commodi maxime pariatur neque laborum possimus.</p>
-            </div>
-        </>
-    )
-  
-}
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(<App/>);
-
-
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(<App />)
