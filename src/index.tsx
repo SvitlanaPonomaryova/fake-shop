bac933d46f7dc17554d7d02bdc9978a37fe2b749
Svitlana Ponomaryova <Svetapriluky@gmail.com>
2022-09-01 21:24:47 +0200

    add header

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './container/App/Aoo'
+import App from 'container/App/Aoo'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(<App />)
