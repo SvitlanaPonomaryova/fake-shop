1235f20d535e17a3b6c40eef1655a58dbcfc29dc
Svitlana Ponomaryova <Svetapriluky@gmail.com>
2022-09-03 16:59:07 +0200

    add ProductList and ProductListItem

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import CssBaseline from '@mui/material/CssBaseline'
 import Header from 'container/Header/Header'
+import Main from 'container/Main/Main'
 
 type Props = {}
 
-const Aoo = () => {
+const App = () => {
     return (
         <>
             <CssBaseline />
             <Header />
+            <Main />
         </>
     )
 }
 
-export default Aoo
+export default App
---
+++ src/container/Main/Main.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container } from '@mui/material'
+import ProductList from 'components/Products/ProductList'
+
+type Props = {}
+
+const Main = (props: Props) => {
+    return (
+        <>
+            <Container>
+                <ProductList />
+            </Container>
+        </>
+    )
+}
+
+export default Main
